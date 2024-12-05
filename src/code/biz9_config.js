cd442fdc32216fd045404196fc4d9621afe49b0b
think1 <think1@localhost.localdomain>
2024-12-05 11:06:41 -0500

    good

--- src/code/biz9_config.js
+++ src/code/biz9_config.js
@@ -1,5 +1,5 @@
-exports.VERSION="1.0.1"
+exports.VERSION="1.0.2"
 exports.TITLE='BiZ9-Data'
 exports.REPO='git@github.com:biz9framework/biz9-data.git'
-exports.BRANCH='1.0.0'
+exports.BRANCH='1.0.1'
 
