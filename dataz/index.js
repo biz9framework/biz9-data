d69baf45b3ab4262886f0b45d1424295aa09f52f
think2 <think2@localhost.localdomain>
2024-12-11 22:56:20 -0500

    Close update and check now working.

--- dataz/index.js
+++ dataz/index.js
@@ -6,22 +6,32 @@
  */
 const path = require('path')
 const biz9_config_file = require(path.join(__dirname, '../../../biz9_config.js'));
+/*
 const get_blank=()=>{
-     return new Promise((resolve) =>{
+    return new Promise((resolve) =>{
+        let error=null;
+        const run = new();
+        run.method().then((data)=> {
+            resolve([null,data]);
+        }).catch(err => handleError)
+        function handleError(error) {
+            error = error;
+            console.error("--Error--"+error.message+"--Error--", error);
+        }
     });
 }
+*/
 const get_db_connect=()=>{
     return new Promise((resolve) =>{
         let error=null;
-        MONGO_FULL_URL="mongodb://"+biz9_config_file.MONGO_USERNAME_PASSWORD+biz9_config_file.MONGO_IP+":"+biz9_config_file.MONGO_PORT+"?retryWrites=true&w=majority&maxIdleTimeMS=60000&connectTimeoutMS=150000&socketTimeoutMS=90000&maxPoolSize=900000&maxConnecting=10000";
-        const { MongoClient } = require("mongodb");
-        const client_db = new MongoClient(MONGO_FULL_URL);
-        client_db.connect().then((result)=> {
-            resolve([null,result]);
+   if(!check_db_connect()){
+          client_db.connect().then((data)=> {
+            resolve(data);
         }).catch(err => handleError)
         function handleError(error) {
             error = error;
             console.error("--Error-Get-DB-Connect--"+error.message+"--Error--", error);
+            throw new Error(error);
             var reset_cmd = "sudo mongod --fork --config "+biz9_config_file.mongo_config;
             error=e;
             if(data_config.mongo_ip!='0.0.0.0'){
@@ -35,12 +45,36 @@ const get_db_connect=()=>{
             dir = exec(reset_cmd, function(error,stdout,stderr){
             });
             dir.on('exit', function (code) {
-                resolve([error,null]);
+                resolve(null);
             });
+        }
+   }else{
+       resolve(null);
+   }
+    });
+}
+const close_db_connect=()=>{
+    return new Promise((resolve) =>{
+        let error=null;
+        client_db.close().then((data)=> {
+            console.log(check_db_connect(client_db.isConnected));
+            resolve(data);
+        }).catch(err => handleError)
+        function handleError(error) {
+            error = error;
+            throw new Error(error);
+            console.error("--Error--"+error.message+"--Error--", error);
+            resolve(error);
         }
     });
 }
+const check_db_connect=()=>{
+        let is_conn = !!client_db && !!client_db.topology && !!client_db.topology.isConnected()
+        return is_conn;
+}
 module.exports = {
     get_db_connect,
+    close_db_connect,
+    check_db_connect
 };
 
--- index.js
+++ index.js
@@ -7,10 +7,21 @@ Description: BiZ9 Framework: Data
 const path = require('path')
 const biz9_config_file = require(path.join(__dirname, '../../biz9_config.js'));
 const dataz = require('./dataz/index.js');
+MONGO_FULL_URL="mongodb://"+biz9_config_file.MONGO_USERNAME_PASSWORD+biz9_config_file.MONGO_IP+":"+biz9_config_file.MONGO_PORT+"?retryWrites=true&w=majority&maxIdleTimeMS=60000&connectTimeoutMS=150000&socketTimeoutMS=90000&maxPoolSize=900000&maxConnecting=10000";
+const { MongoClient } = require("mongodb");
+client_db = new MongoClient(MONGO_FULL_URL);
 const get_client_db = async () => {
-    console.log('aaaaaaa');
-    return [error,connect] = await dataz.get_db_connect();
+    return data = await dataz.get_db_connect();
+};
+const close_client_db = async () => {
+    return data = await dataz.close_db_connect();
+};
+const check_client_db = async () => {
+    return data = await dataz.check_db_connect();
 };
 module.exports = {
     get_client_db,
+    close_client_db,
+    check_client_db
 };
+
