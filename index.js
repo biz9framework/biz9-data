f4161b274757dd20ea8e9ebabab942562325455c
think1 <think1@localhost.localdomain>
2026-02-17 22:18:00 -0500

    good

--- index.js
+++ index.js
@@ -13,6 +13,149 @@ const {Foreign} = require('./foreign.js');
 const {Group} = require('./group.js');
 const {Join} = require('./join.js');
 const {Adapter}  = require('./adapter.js');
+class Search_Type {
+    //type - search
+    static ITEMS='items';
+    static ONE='one';
+    static COUNT='count';
+}
+class Data_Field{
+    //result
+    static RESULT_OK = 'resultOK';
+    static RESULT_OK_DELETE = 'delete_resultOK';
+    static RESULT_OK_GROUP_DELETE = 'group_delete_resultOK';
+    static RESULT_OK_GROUP_IMAGE_DELETE = 'group_image_delete_resultOK'; //remove
+    static RESULT_OK_IMAGE_DELETE = 'image_delete_resultOK'; //remove
+
+    static RESULT_OK_DELETE_CACHE = 'delete_cache_resultOK';
+    static RESULT_OK_DELETE_DATABASE = 'delete_database_resultOK';
+    static SOURCE='source';
+    static SOURCE_DATA_TYPE='source_data_type';
+    static SOURCE_KEY='source_key';
+    static SOURCE_ID='source_id';
+    static SOURCE_PARENT_ID='source_parent_id';
+    //sort_by
+    static SORT_BY_ASC='asc';
+    static SORT_BY_DESC='desc';
+    //data_source
+    static DATA_SOURCE_CLIENT_CACHE="client_cache";
+    static DATA_SOURCE_SERVER_CACHE="server_cache";
+    static DATA_SOURCE_DATABASE="database";
+    static DATA_SOURCE_CLIENT="client";
+    static DATA_SOURCE_SERVER="server";
+    static DATA_SOURCE_NOT_FOUND="not_found";
+
+    //static FIELD_GROUP_ID='group_id';
+}
+class Data_Title{
+    //general
+    static GROUP='Group';
+    static N_A='N/A';
+    //source
+    static SOURCE_DATABASE='Database';
+    static SOURCE_CACHE='Cache';
+    static SOURCE_NOT_FOUND='Not-Found';
+}
+class Data_Type {
+    static BLANK='blank_biz';
+    static GROUP='group_biz';
+}
+class Data_Logic {
+    static get_new = (data_type,id,option) => {
+        return Data_Logic.get(data_type,id,option);
+    };
+    static get_search = (data_type,filter,sort_by,page_current,page_size,option) => {
+        option = !Obj.check_is_empty(option)  ? option : {};
+        return {data_type:data_type,filter:filter,sort_by:sort_by,page_current:page_current,page_size:page_size,option:option};
+    }
+    static get_search_group = (option) => {
+        option = !Obj.check_is_empty(option)  ? option : {};
+        let type  = option.type ? option.type : Type.SEARCH_ITEMS;
+        let field = option.field ? option.field : {};
+        let title = option.title ? Str.get_title_url(option.title) : {};
+        let image = option.image ? option.image : false;
+        let page_current = option.page_current ? option.page_current : 1;
+        let page_size = option.page_size ? option.page_size : 0;
+        return {type:type,field:field,title:title,image:image,page_current:page_current,page_size:page_size};
+    }
+    static get_search_foreign = (type,foreign_data_type,foreign_field,parent_field,option) => {
+        option = !Obj.check_is_empty(option)  ? option : {};
+        type = type ? type : Type.SEARCH_ITEMS;
+        foreign_data_type = foreign_data_type ? foreign_data_type : Str.get_title_url(Data_Logic.get_data_type_by_type(foreign_data_type,{plural:true}));
+        foreign_field = foreign_field ? foreign_field : Type.FIELD_PARENT_ID;
+        parent_field = parent_field ? parent_field : parent_field;
+        let field = option.field ? option.field : null;
+        let title = option.title ? Str.get_title_url(option.title) : Str.get_title_url(Data_Logic.get_data_type_by_type(foreign_data_type,{plural:true}));
+        let page_current = option.page_current ? option.page_current : 1;
+        let page_size = option.page_size ? option.page_size : 0;
+        return {type:type,foreign_data_type:foreign_data_type,foreign_field:foreign_field,parent_field:parent_field,type:type,field:field,title:title,page_current:page_current,page_size:page_size};
+    }
+    static get_search_join = (type,search,option) => {
+        option = !Obj.check_is_empty(option)  ? option : {};
+        type = type ? type : Type.SEARCH_ITEMS;
+        search = search ? search : Data_Logic.get_search(Type.DATA_BLANK,{},{},1,0);
+        let field = option.field ? option.field : {};
+        let distinct = option.distinct ? option.distinct : null; //distinct:{field:'title',sort_by:Type.SEARCH_SORT_BY_DESC}
+        let title = option.title ? Str.get_title_url(option.title) : Str.get_title_url(Data_Logic.get_data_type_by_type(search.data_type,{plural:true}));
+        let foreigns = option.foreigns ? option.foreigns : [];
+        let page_current = option.page_current ? option.page_current : 1;
+        let page_size = option.page_size ? option.page_size : 0;
+        return {type:type,search:search,field:field,title:title,distinct:distinct,foreigns:foreigns,page_current:page_current,page_size:page_size};
+    }
+    static copy = (data_type,item,option)=>{
+        let copy_item = Data_Logic.get_new(data_type,0);
+        option = !Obj.check_is_empty(option)  ? option : {};
+        const keys = Object.keys(item);
+        keys.forEach(key => {
+            if(
+                key!=Type.FIELD_ID&&
+                key!=Type.FIELD_SOURCE&&
+                key!=Type.FIELD_DATE_CREATE&&
+                key!=Type.FIELD_DATE_SAVE&&
+                key!=Type.TITLE_OBJ&&
+                key!=Type.TITLE_USER&&
+                key!=Type.TITLE_GROUP&&
+                key!=Type.TITLE_ITEM&&
+                !Obj.check_is_array(item[key])&&
+                Obj.check_is_value(item[key])
+            ){
+                copy_item[key]=item[key];
+            }
+        });
+        return copy_item;
+    };
+    static get_not_found = (data_type,id,option) =>{
+        option=!Obj.check_is_empty(option)?option:{};
+        if(!data_type){
+            data_type = Type.DATA_BLANK;
+        }
+        if(!id){
+            id = 0;
+        }
+        if(data_type != Type.DATA_USER){
+            if(!id){
+                id=0;
+            }
+            let item = Data_Logic.get(data_type,id);
+            item.id = 0;
+            item.id_key = id;
+            item.title = "Item Not Found";
+            item.title_url = Str.get_title_url(item.title);
+            item.source = Type.TITLE_SOURCE_NOT_FOUND;
+            return item;
+        }else{
+            let user = Data_Logic.get(data_type,id);
+            user.id = 0;
+            user.id_key = id;
+            user.title = "User Not Found";
+            user.first_name = "User Not Found";
+            user.title_url = Str.get_title_url(user.title);
+            user.source = Type.TITLE_SOURCE_NOT_FOUND;
+            return user;
+        }
+    };
+
+}
 class Database {
     static get = async (data_config,option) => {
         /* return
@@ -1994,7 +2137,7 @@ class Stat_Data {
                         data.item_count = biz_data.item_count;
                         data.page_count = biz_data.page_count;
                         data.search = biz_data.search;
-                        data.stats = biz_data[Type.FIELD_ITEMS];
+                        data.stats = biz_data[Data_Type.FIELD_ITEMS];
                     }
                 },
             ]).then(result => {
@@ -2012,7 +2155,7 @@ class Blank_Data {
         return new Promise((callback) => {
             let error = null;
             let cache = {};
-            let data = Data_Logic.get(Type.DATA_BLANK,0);
+            let data = Data_Logic.get(Data_Type.BLANK,0);
             let option = {};
             get_blank(database,data.data_type,data.id,option).then(([biz_error,biz_data])=>{
                 if(biz_error){
@@ -2036,7 +2179,7 @@ class Blank_Data {
         return new Promise((callback) => {
             let error = null;
             let cache = {};
-            let data = Data_Logic.get(Type.DATA_BLANK,0);
+            let data = Data_Logic.get(Data_Type.BLANK,0);
             let option = {};
             //top
             async.series([
